b3afef99651661d22b97a4f85a79b67d0605e3d1
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-11-21 16:55:36 -0700

    Exercise completed.

--- ArrayListExerciseTwo/Program.cs
+++ ArrayListExerciseTwo/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter in your name");
+            var userName = Console.ReadLine();
+            var nameArray = userName.ToCharArray();
+            var reverseNameArray = nameArray.Reverse();
+            string reverseName = string.Join("", reverseNameArray);
+            Console.WriteLine(string.Format("Your name spelled backwards is: {0}.", reverseName));
         }
     }
 }
